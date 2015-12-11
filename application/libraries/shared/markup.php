c894e2283829f3e915ab67bf079587b689b68dc5
Hemant Mann <hemant.mann121@gmail.com>
2015-12-11 20:42:58 +0530

    Added SwiftMVC

---
+++ application/libraries/shared/markup.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Description of markup
+ *
+ * @author Faizan Ayubi
+ */
+
+namespace Shared {
+
+    class Markup {
+
+        public function __construct() {
+            // do nothing
+        }
+
+        public function __clone() {
+            // do nothing
+        }
+
+        public static function errors($array, $key, $separator = "<br />", $before = "<br />", $after = "") {
+            if (isset($array[$key])) {
+                return $before . join($separator, $array[$key]) . $after;
+            }
+            return "";
+        }
+
+        public static function pagination($page) {
+            if (strpos(URL, "?")) {
+                $request = explode("?", URL);
+                if (strpos($request[1], "&")) {
+                    parse_str($request[1], $params);
+                }
+
+                $params["page"] = $page;
+                return $request[0]."?".http_build_query($params);
+            } else {
+                $params["page"] = $page;
+                return URL."?".http_build_query($params);
+            }
+            return "";
+        }
+
+    }
+
+}
