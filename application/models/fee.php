c94a64fd85c6cf819878165819c18e0aa5d06981
Faizan Ayubi <indianayubi@gmail.com>
2016-01-07 05:21:25 +0530

    Fee management done

--- application/models/fee.php
+++ application/models/fee.php
@@ -5,5 +5,69 @@
  * @author Hemant Mann
  */
 class Fee extends Shared\Model {
+	
+	/**
+     * @column
+     * @readwrite
+     * @type integer
+     * @index
+     *
+     * @validate required, numeric
+     */
+    protected $_user_id;
 
+    /**
+     * @column
+     * @readwrite
+     * @type integer
+     * @index
+     *
+     * * @validate required, numeric
+     */
+    protected $_organization_id;
+
+    /**
+     * @column
+     * @readwrite
+     * @type text
+     * @length 100
+     * 
+     * @validate required, min(3), max(100)
+     */
+    protected $_category;
+
+    /**
+     * @column
+     * @readwrite
+     * @type text
+     */
+    protected $_description;
+
+    /**
+     * @column
+     * @readwrite
+     * @type date
+     *
+     * @validate required
+     */
+    protected $_start;
+
+    /**
+     * @column
+     * @readwrite
+     * @type date
+     *
+     * @validate required
+     */
+    protected $_end;
+
+    /**
+     * @column
+     * @readwrite
+     * @type decimal
+     * @length 10,2
+     *
+     * @validate required
+     */
+    protected $_amount;
 }
--- application/models/feeperiod.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * The Fee Period Model
- *
- * @author Hemant Mann
- */
-class FeePeriod extends Shared\Model {
-
-}
