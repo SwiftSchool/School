57092d336ad04a0d8af07ed8bd66e07a6ecb817c
Faizan Ayubi <indianayubi@gmail.com>
2016-01-06 12:51:19 +0530

    Fee Management done

--- application/controllers/payment.php
+++ application/controllers/payment.php
@@ -10,5 +10,39 @@ use Framework\Registry as Registry;
 use Framework\ArrayMethods as ArrayMethods;
 
 class Payment extends School {
+	
+	/**
+	 * @before _secure, _school
+	 */
+	public function createfee() {
+		$this->setSEO(array("title" => "Fee | School"));
+		$view = $this->getActionView();
 
+		$grades = \Grade::all(array("organization_id = ?" => $this->organization->id), array("id", "title"));
+		$view->set("grades", $grades);
+	}
+
+	/**
+	 * @before _secure, _school
+	 */
+	public function records() {
+		$this->setSEO(array("title" => "Manage Payment | School"));
+		$view = $this->getActionView();
+
+		$grades = Grade::all(array("organization_id = ?" => $this->organization->id), array("id", "title"));
+		$view->set("grades", $grades);
+		$view->set("courses", array());
+		$view->set("enrollments", array());
+	}
+
+	/**
+	 * @before _secure, _school
+	 */
+	public function add() {
+		$this->setSEO(array("title" => "Add Payment | School"));
+		$view = $this->getActionView();
+
+		$grades = \Grade::all(array("organization_id = ?" => $this->organization->id), array("id", "title"));
+		$view->set("grades", $grades);
+	}
 }
